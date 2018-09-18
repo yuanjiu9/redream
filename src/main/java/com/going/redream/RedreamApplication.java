db3c5cbd11b816e19f0353532a44e4b2e4e2f461
yuandg <yuandg@shishike.com>
2018-09-18 22:59:18 +0800

    Initial commit

---
+++ src/main/java/com/going/redream/RedreamApplication.java
@@ -0,0 +1,12 @@
+package com.going.redream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedreamApplication.class, args);
+    }
+}
